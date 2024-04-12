ff6c13429ce2782844bff54dfe3519fbc0c5fcf0
Braian Mena <braianmena@gmail.com>
2024-04-12 11:05:07 -0300

    Implementacion de 'user.controllers' - Modificacion de 'index', 'user.routes' y 'users.model'(modificacion de atributos anidados).

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import UserModel from "../models/users.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 //Get all users
 
@@ -10,4 +12,145 @@ const getAllUsers = async (req, res) => {
     console.log(error);
   }
 };
-export default getAllUsers;
+
+const createUser = async (req, res) => {
+  try {
+    const { nombre, apellido, email, password, admin, isAprobado } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const passHash = await bcrypt.hash(password, salt);
+
+    const newUser = UserModel({
+      nombre,
+      apellido,
+      email,
+      password: passHash,
+      admin,
+      isAprobado
+    });
+
+    const user = await newUser.save();
+    res.json(user);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json( {message: error} );
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if(!email || !password){
+      return res.status(400).json( {message: "Debe rellenar todos los campos"} );
+    };
+
+    const user = await UserModel.findOne( {email} );
+
+    if(!user) {
+      return res.status(404).json( {message: "Usuario o contraseña incorrecta."});
+    };
+
+    const validatePass = await bcrypt.compare(password, user.password);
+
+    if(!validatePass){
+      return res.status(404).json( {message: "Usuario o contraseña incorrecta."});
+    };
+
+    const token =  jwt.sign(
+      {
+        id: user._id,
+        nombre: user.nombre,
+        apellido: user.apellido,
+        admin: user.admin,
+        isAprobado: user.isAprobado
+      },
+      process.env.SECRET,
+      { expiresIn: "1h"}
+    );
+
+    res.header(token).json( {token} );
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json( {message: error} );
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre, apellido, admin } = req.body; // (?) que pasa si se quiere reestablecer la pass?
+    const updateUser = await UserModel.findByIdAndUpdate(
+      id,
+      { nombre, apellido, admin },
+      { new: true }
+    );
+
+    res.json(updateUser);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json( {message: error} );
+  }
+};
+
+const deleteUser = async (req, res) => {
+  console.log(req, "<--req DELETE");
+  try {
+    const { id } = req.params;
+    const user = await UserModel.findByIdAndDelete(id);
+
+    res.json( {message: "Usuario Eliminado."});
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json( {message: error} );
+  }
+
+};
+
+const getUser = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const user = await UserModel.findOne({ email });
+    
+
+    if(!user){
+      return res.status(404).json( {message: "No se encontro coincidencias."} );
+    }
+
+    res.json(user);
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json( {message: error} );
+  }
+  
+};
+
+const disableUser = async (req, res) => {  
+  try {
+    const { id } = req.params;
+    const updateUser = await UserModel.findByIdAndUpdate(
+      id,
+      { isAprobado: false },
+      { new: true }
+    );
+    
+    res.json(updateUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json( {message: error} );
+  }
+};
+
+export default {
+  getAllUsers,
+  createUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+  getUser,
+  disableUser
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,6 +6,23 @@ const router = express.Router();
 //Get all users
 router.get("/usuarios", userController.getAllUsers);
 
+/* COMENTARIO NECESARIO PARA PODER PROBAR LOS CONROLLERS
+// Register
+router.post("/register", userController.createUser);
 
+// Login
+router.post("/login", userController.loginUser);
 
+//Update
+router.put("/update/:id", userController.updateUser);
+
+//Delete
+router.delete("/delete/:id", userController.deleteUser);
+
+//Get user
+router.get("/user/:email", userController.getUser);
+
+//Disable user
+router.put("/disable/:id", userController.disableUser);
+*/
 export default router; 
