ee2e79fd4f8e5f3608f65784fef473b0b4889dcd
FernandezCristian <fernandezcristian.utn@gmail.com>
2024-04-14 10:46:07 -0300

    Rutas terminada

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,24 +3,16 @@ import userController from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-//Get all users
+
 router.get("/usuarios", userController.getAllUsers);
 
-
-//Registro
 router.post("/registro", userController.registroUsuario);
 
-// Delete user
 router.delete("/delete/:id", userController.deleteUsuario);
-
-// Update user
 
 router.put("/update/:id", userController.updateUser);
 
-// Login
-
 router.post("/login", userController.loginUsuario);
-
 
 
 export default router; 
